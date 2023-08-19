82d54687f924b88d932979b0ede9b2ffc8ae6151
Matan Eliyahu <mataneliyahu@gmail.com>
2023-08-19 15:20:50 +0300

    add dotenv

--- src/routes/homaPage.ts
+++ src/routes/homaPage.ts
@@ -3,7 +3,8 @@ import * as express from 'express';
 const router = express.Router();
 
 
-async function addComment(req: express.Request, res: express.Response){       
+async function addComment(req: express.Request, res: express.Response){
+      console.log("new request")       
       res.status(201).json("im here");
 };
 
