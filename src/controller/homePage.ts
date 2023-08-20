828f5b19f3240d1d89e5dfc3c15c8149223075d2
Matan Eliyahu <mataneliyahu@gmail.com>
2023-08-20 19:44:40 +0300

    fix the api

--- src/controller/homePage.ts
+++ src/controller/homePage.ts
@@ -5,10 +5,10 @@ const apiUrl = process.env.MOVIE_PNG_PREFIX;
 
 export const getHomePage = async (req: Request, res: Response) => {
   try {
-    const movies = await axios.get(`${apiUrl}${process.env.POPULAR}ai_key=${process.env.TMDB_KEY}`);
+    const movies = await axios.get(`${apiUrl}${process.env.POPULAR}api_key=${process.env.TMDB_KEY}`);
     res.status(200).send(movies.data);
 
   } catch (error) {
-    return res.status(500).json({error: ""});
+    return res.status(500).json({error: "Could not get the Movies"});
   }
 };
